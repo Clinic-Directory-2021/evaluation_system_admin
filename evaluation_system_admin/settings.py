1c75377f467dbc60670f198f9b5a4da14b02f644
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2021-11-11 16:44:57 +0800

    commit

--- evaluation_system_admin/settings.py
+++ evaluation_system_admin/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-pec)tg)fz@grj(7gnxlsddrx5q$600roymwh(lpq$*^(71l6g7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['evaluationsystem-deped.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','evaluationsystem-deped.herokuapp.com']
 
 
 # Application definition
