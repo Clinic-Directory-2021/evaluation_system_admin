01d5ed96a22ddc697c56fb3ec294ba8d06399172
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-02 00:22:54 +0800

    asdasdasd

--- main_app/functions.py
+++ main_app/functions.py
@@ -1,9 +1,9 @@
 def get_mean(evaluate_dict, total_of_participant):
-    output = 0
+    temp = 0
     ctr = 4
     for value in evaluate_dict.values():
         if value != 0:
-            output += value * ctr
+            temp += value * ctr
         ctr = ctr - 1
-    output = output / total_of_participant 
+    output = temp / total_of_participant 
     return output
