c09e6bd8f34a124213013f054e048208cf63ff19
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-01 13:56:55 +0800

    sadasdsa

--- main_app/functions.py
+++ main_app/functions.py
@@ -1,6 +1,6 @@
 def iterate(dictionary):
     new_list = []
-    for key, value in dictionary.iteritems():
+    for key, value in dictionary.items():
         temp = [key,value]
         new_list.append(temp)
     return new_list
