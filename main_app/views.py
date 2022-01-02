d333c7e98c31cda6aa74e4577ec93b0a28208650
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-02 13:35:31 +0800

    asdsadsdsa

--- main_app/views.py
+++ main_app/views.py
@@ -1264,7 +1264,7 @@ def save_summary(request):
                 temp_dict = {}
                 temp_dict[facilitator_ctr] = facilitators_data.to_dict()
                 facilitator_dict[evaluation_data.id] = temp_dict
-            facilitator_ctr += 1
+                facilitator_ctr += 1
         total_of_participant += 1
         q1 = u'{}'.format(data.to_dict()['q1'])
         q2 = u'{}'.format(data.to_dict()['q2'])
