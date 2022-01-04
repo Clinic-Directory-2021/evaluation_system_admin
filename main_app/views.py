32748bc95e38321f5259919d245fea079a8d7268
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-04 19:19:03 +0800

    sadasdasdd

--- main_app/views.py
+++ main_app/views.py
@@ -1279,7 +1279,7 @@ def save_summary(request):
         for evaluator_data in evaluators_data:
             facilitators = evaluators.document(evaluator_data.id).collection('facilitators').get()
             for facilitators_data in facilitators:
-                sss = facilitators_data.get()
+                sss = facilitators_data.to_dict()
                 facilitator_ctr += 1
         total_of_participant += 1
         q1 = u'{}'.format(data.to_dict()['q1'])
