b3a6cd54a4a884c812d53e7bcb59f32c46c04bc6
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-06 00:09:12 +0800

    asdsadsadad

--- main_app/views.py
+++ main_app/views.py
@@ -1308,9 +1308,9 @@ def save_summary(request):
                 # q16 = u'{}'.format(facilitators_data.to_dict()['q16'])
                 # q17 = u'{}'.format(facilitators_data.to_dict()['q17'])
                 # test[evaluator_data.id] = facilitators_data.to_dict()
-                for key1,facilitator_response_data in facilitator_response.items():
+                for facilitator_response_data in facilitator_response.values():
                     for key2,facilitator_response_data2 in facilitator_response_data.items():
-                                sss = key2
+                            if key2 == "q9":
                                 facilitator_response_data2["4"] += 1 
 
                         
