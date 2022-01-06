f5b0096ffd1688a424bfd93eb98a7e5a824746ca
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-07 07:39:38 +0800

    asddsaasdsa

--- main_app/functions.py
+++ main_app/functions.py
@@ -16,7 +16,7 @@ def get_mean(evaluate_dict, total_of_participant):
 def get_facilitator_rate(response_dictionary,facilitator_id, data, key):
     response_dictionary[facilitator_id][key][data] += 1/2
 
-def get_facilitator_mean(response_dictionary,mean_dictionary,total_of_participants):
+def get_facilitator_mean(response_dictionary,total_of_participants):
     for key1, data1 in response_dictionary.items():
         for key2, data2 in data1.items():
             output = 0
@@ -27,15 +27,14 @@ def get_facilitator_mean(response_dictionary,mean_dictionary,total_of_participan
                     output += (data3 * int(key3))
 
 def facilitator_overall_mean(mean_dictionary,response_dictionary):
-    list = []
+    my_list = []
     for key1, data1 in response_dictionary.items():
             for key2, data2 in data1.items():
-                output = 0
                 for key3, data3 in data2.items():
                     if key3 ==  "mean":
-                        list.append[data3]
-            mean_dictionary[key1] = statistics.mean(list)
-            list.clear()
+                        my_list.append[data3]
+            mean_dictionary[key1] = statistics.mean(my_list)
+            my_list.clear()
                         
 
 
