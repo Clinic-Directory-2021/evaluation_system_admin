b67e0196b360999e6c1a4958004a3a1d2a834fe2
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-06 17:46:10 +0800

    sadasd

--- main_app/functions.py
+++ main_app/functions.py
@@ -8,8 +8,7 @@ def get_mean(evaluate_dict, total_of_participant):
     return output / total_of_participant
 
 def get_facilitator_rate(response_dictionary,facilitator_id, data, key):
-    
-    response_dictionary[facilitator_id][key][data] += 1
+    response_dictionary[facilitator_id][key][data] += 1/2
 
 
 
