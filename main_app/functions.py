8e388c3b43e26606ec5ffdc41e5adaab303caf08
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-06 16:41:46 +0800

    asdasdasdas

--- main_app/functions.py
+++ main_app/functions.py
@@ -10,9 +10,16 @@ def get_mean(evaluate_dict, total_of_participant):
 def get_facilitator_rate(response_dictionary,facilitator_id, data, key):
     response_dictionary[facilitator_id] = {
         "q9":{"4":0,"3":0,"2":0,"1":0},
+        "q10":{"4":0,"3":0,"2":0,"1":0},
+        "q11":{"4":0,"3":0,"2":0,"1":0},
+        "q12":{"4":0,"3":0,"2":0,"1":0},
+        "q13":{"4":0,"3":0,"2":0,"1":0},
+        "q14":{"4":0,"3":0,"2":0,"1":0},
+        "q15":{"4":0,"3":0,"2":0,"1":0},
+        "q16":{"4":0,"3":0,"2":0,"1":0},
+        "q17":{"4":0,"3":0,"2":0,"1":0},
     }
-    if key == "q9":
-        response_dictionary[facilitator_id][key][data] = key
+    response_dictionary[facilitator_id][key][data] = key
 
 
 
