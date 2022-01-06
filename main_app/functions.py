b4bb9add3a88f5b8dea3e3db90e493797b1dc1d5
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-06 15:31:38 +0800

    asdsadasd

--- main_app/functions.py
+++ main_app/functions.py
@@ -6,3 +6,13 @@ def get_mean(evaluate_dict, total_of_participant):
             output += value * ctr
         ctr = ctr - 1
     return output / total_of_participant
+
+def get_facilitator_rate(response_dictionary,get_q9,facilitator_id, data):
+    response_dictionary[facilitator_id] = {
+        "q9":{"4":0,"3":0,"2":0,"1":0},
+    }
+    for key1, value1 in response_dictionary.items():
+        for key2, value2 in value1.items():
+            if()
+
+
