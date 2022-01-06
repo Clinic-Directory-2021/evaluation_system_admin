22f98667f0775e25554f3a9fc27bfd1de984b520
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-06 18:25:26 +0800

    asdasdasd

--- main_app/functions.py
+++ main_app/functions.py
@@ -13,6 +13,7 @@ def get_facilitator_rate(response_dictionary,facilitator_id, data, key):
     response_dictionary[facilitator_id][key][data] += 1/2
 
 def get_facilitator_mean(response_dictionary,mean_dictionary,total_of_participants):
+    variable = total_of_participants
     for key1,value1 in response_dictionary.items():
         for key2,value2 in value1.items():
             mean_dictionary[key1][key2] += value2   
