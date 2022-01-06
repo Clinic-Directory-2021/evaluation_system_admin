caee4ed46b6d59732b9515df9d97bd9f2b624cd8
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-06 17:27:40 +0800

    asdasddsaasdsadsda

--- main_app/functions.py
+++ main_app/functions.py
@@ -9,10 +9,11 @@ def get_mean(evaluate_dict, total_of_participant):
 
 def get_facilitator_rate(response_dictionary,facilitator_id, data, key):
     
-    if response_dictionary[facilitator_id][key][data]:
-        response_dictionary[facilitator_id][key][data] = 1
+    if key in response_dictionary[facilitator_id].keys():
+        if data in response_dictionary[facilitator_id][key].keys():
+            response_dictionary[facilitator_id][key][data] += 1
     else:
-        response_dictionary[facilitator_id][key][data] += 1
+        response_dictionary[facilitator_id][key][data] = 1
 
 
 
