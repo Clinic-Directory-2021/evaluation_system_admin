fba35c826c34014e292504a1fb1c6cb4ed5f8f20
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2022-01-29 03:20:31 +0800

    asdasd

--- main_app/functions.py
+++ main_app/functions.py
@@ -17,6 +17,8 @@ def get_mean(evaluate_dict, total_of_participant):
 def get_facilitator_rate(response_dictionary,facilitator_id, data, key):
         if key == "exist" or key == "facilitator_name":
             print()
+        elif key == "topic":
+            response_dictionary[facilitator_id][key][data] += data
         else:
             response_dictionary[facilitator_id][key][data] += 1
 
@@ -25,7 +27,7 @@ def get_facilitator_mean(response_dictionary,total_of_participants):
         for key2, data2 in data1.items():
             output = 0
             for key3, data3 in data2.items():
-                if key3 ==  "mean":
+                if key3 ==  "5":
                     response_dictionary[key1][key2][key3] = output / total_of_participants 
                 else:
                     output += (data3 * int(key3))
@@ -35,7 +37,7 @@ def facilitator_overall_mean(mean_dictionary,response_dictionary):
     for key1, data1 in response_dictionary.items():
             for key2, data2 in data1.items():
                 for key3, data3 in data2.items():
-                    if key3 ==  "mean":
+                    if key3 ==  "5":
                         my_list.append(data3)
             mean_dictionary[key1] = round(statistics.mean(my_list),1)
             my_list.clear()
